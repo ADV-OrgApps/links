b297701a15e6f1c666fbd16d341d6034f220b1e6
destinl27 <124813431+destinl27@users.noreply.github.com>
2026-05-01 15:07:45 -0500

    Update report names in redirect map to use underscores

--- function_app.py
+++ function_app.py
@@ -9,10 +9,10 @@ def link_redirect(req: func.HttpRequest) -> func.HttpResponse:
 
     # Dictionary mapping your clean URLs to the Fabric Org App GUIDs
     redirect_map = {
-        "digital-annual-report": "https://app.fabric.microsoft.com/groups/me/orgapps/c994d0ee-cdd9-43d3-8892-59ff978b3621?ctid=d8999fe4-76af-40b3-b435-1d8977abc08c",
-        "mobile-annual-report": "https://app.fabric.microsoft.com/groups/me/orgapps/4c8b1742-8ce8-41b2-ab71-384bb0626d43?ctid=d8999fe4-76af-40b3-b435-1d8977abc08c",
-        "print-annual-report": "https://app.fabric.microsoft.com/groups/me/orgapps/93c11ebb-fbff-46c1-b10b-34870726350d?ctid=d8999fe4-76af-40b3-b435-1d8977abc08c",
-        "static-digital-annual-report": "https://app.fabric.microsoft.com/groups/me/orgapps/04811eda-41d2-4bb7-b6b3-c65d1bedbc71?ctid=d8999fe4-76af-40b3-b435-1d8977abc08c"
+        "Digital_Annual_Report": "https://app.fabric.microsoft.com/groups/me/orgapps/c994d0ee-cdd9-43d3-8892-59ff978b3621?ctid=d8999fe4-76af-40b3-b435-1d8977abc08c",
+        "Mobile_Annual_Report": "https://app.fabric.microsoft.com/groups/me/orgapps/4c8b1742-8ce8-41b2-ab71-384bb0626d43?ctid=d8999fe4-76af-40b3-b435-1d8977abc08c",
+        "Print_Annual_Report": "https://app.fabric.microsoft.com/groups/me/orgapps/93c11ebb-fbff-46c1-b10b-34870726350d?ctid=d8999fe4-76af-40b3-b435-1d8977abc08c",
+        "Static_Digital_Annual_Report": "https://app.fabric.microsoft.com/groups/me/orgapps/04811eda-41d2-4bb7-b6b3-c65d1bedbc71?ctid=d8999fe4-76af-40b3-b435-1d8977abc08c"
     }
 
     # Fetch the destination URL
